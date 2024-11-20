a1a2106b2115d96b5f66997c5abbced772397314
Kaleb Missmer <143435924+kmissmer@users.noreply.github.com>
2024-11-20 10:03:15 -0500

    Update Delete_ShelvedBooks_Lambda.sh

--- Delete_ShelvedBooks_Lambda.sh
+++ Delete_ShelvedBooks_Lambda.sh
@@ -1,6 +1,5 @@
 if aws lambda delete-function --function-name shelvedBooksFunction >/dev/null 2>&1 ; then
     echo "DONE"
-    rm shelvedBooksFunction.zip
 else
     echo "Function does not exist"
 fi
