61fcd09559118d37a2dd6d17c9c16888fee502b4
Kaleb Missmer <143435924+kmissmer@users.noreply.github.com>
2024-11-20 10:02:54 -0500

    Update Delete_SearchBooks_Lambda.sh

--- Delete_SearchBooks_Lambda.sh
+++ Delete_SearchBooks_Lambda.sh
@@ -1,6 +1,5 @@
 if aws lambda delete-function --function-name searchBooksFunction >/dev/null 2>&1 ; then
     echo "DONE"
-    rm searchBooksFunction.zip
 else
     echo "Function does not exist"
 fi
