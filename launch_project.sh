f1a199bf2e3bf9565eafa411310e0a24a3dfb034
cadizsd <143456301+cadizsd@users.noreply.github.com>
2024-11-22 03:06:14 -0500

    Create launch_project.sh

---
+++ launch_project.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+chmod +x create_dynamodb.sh
+chmod +x create_saved_books_lambda.1.sh
+chmod +x create_search_books_lambda.sh
+chmod +x create_shelved_books_lambda.sh
+chmod +x deploy.sh
+chmod +x update_index.sh
+
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+./create_dynamodb.sh
+
+./create_saved_books_lambda.1.sh
+
+./create_search_books_lambda.sh
+
+./create_shelved_books_lambda.sh
+
+python create_books_api.py
+
+./deploy.sh
+
+./update_index.sh
