3ca431d23e9c0c5d36ed9a9f1004c3ad03faf907
Kaleb Missmer <143435924+kmissmer@users.noreply.github.com>
2024-12-05 22:29:52 -0500

    Create sns_teardown.sh

---
+++ sns_teardown.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Get the SNS topic ARN dynamically
+SNS_TOPIC_ARN=$(aws sns list-topics --query "Topics[?contains(TopicArn, 'ReadingTracker')].TopicArn" --output text)
+
+# Get the Lambda function name dynamically
+LAMBDA_FUNCTION_NAME=$(aws lambda list-functions --query "Functions[?contains(FunctionName, 'ReadingTrackerNotification')].FunctionName" --output text)
+
+# Get the CloudWatch rule ARN dynamically
+CLOUDWATCH_RULE_ARN=$(aws events list-rules --query "Rules[?contains(Name, 'ReadingTrackerRule')].Arn" --output text)
+
+# Delete SNS topic if it exists
+if [ "$SNS_TOPIC_ARN" != "None" ]; then
+  aws sns delete-topic --topic-arn "$SNS_TOPIC_ARN"
+  echo "SNS topic deleted."
+else
+  echo "SNS topic does not exist."
+fi
+
+# Delete Lambda function if it exists
+if [ "$LAMBDA_FUNCTION_NAME" != "None" ]; then
+  aws lambda delete-function --function-name "$LAMBDA_FUNCTION_NAME"
+  echo "Lambda function deleted."
+else
+  echo "Lambda function does not exist."
+fi
+
+# Delete CloudWatch rule if it exists
+if [ "$CLOUDWATCH_RULE_ARN" != "None" ]; then
+  aws events remove-targets --rule "ReadingTrackerRule" --target-ids "LambdaTarget"
+  aws events delete-rule --name "ReadingTrackerRule"
+  echo "CloudWatch rule deleted."
+else
+  echo "CloudWatch rule does not exist."
+fi
