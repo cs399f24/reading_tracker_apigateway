0a5053c06031800d0f65cd4f003d7156d896ec56
Kaleb Missmer <143435924+kmissmer@users.noreply.github.com>
2024-12-05 21:19:12 -0500

    Update sns_setup.sh

--- sns_setup.sh
+++ sns_setup.sh
@@ -10,6 +10,6 @@ python3 subscribe_user_email.py
 
 # Run the Python script to create a CloudWatch rule to trigger SNS every 5 minutes
 echo "Creating CloudWatch Rule..."
-python3 create_cloudwatch_rule.py
+python3 cloudwatch_sns_rule.py
 
 echo "SNS Setup Complete."
