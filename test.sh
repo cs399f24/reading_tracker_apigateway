d37a12925dec9288591ae87c18229e5e6fe1cac6
cadizsd <143456301+cadizsd@users.noreply.github.com>
2024-12-10 01:17:09 -0500

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-API_ID=$(aws apigateway get-rest-apis --query "items[?name=='BooksAPI'].id" --output text)
-URL="https://$API_ID.execute-api.us-east-1.amazonaws.com/dev"
-curl -X POST "$URL/save_book" \
--H 'Content-Type: application/json' \
--d '{"BookID": "123456800", "Title": "Sample Book 2", "Author": "John Doe", "PageCount": 100}'
-
